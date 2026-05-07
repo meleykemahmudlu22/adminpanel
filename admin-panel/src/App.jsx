7cdb521f1b9a46f2eb3db96c51ee39014d543b5f
meleykemahmudlu22 <meleykemahmudlu03@gmail.com>
2026-05-07 16:02:38 +0400

    admindashboard

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Box from './assets/components/Box/Box'
+import Admin from './assets/components/admin/Admin'
 
 const App = () => {
   return (
     <div>
       <Box/>
+      <Admin/>
     </div>
   )
 }
---
+++ admin-panel/src/assets/components/admin/Admin.jsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import { FaBookAtlas } from "react-icons/fa6";
+import { FaBook } from "react-icons/fa6";
+import { IoIosBook } from "react-icons/io";
+import { IoIosBookmark } from "react-icons/io";
+import { MdModeEdit } from "react-icons/md";
+import { AiFillDelete } from "react-icons/ai";
+import "./admin.css"
+
+const Admin = () => {
+  return (
+    <div>
+      <div className="AdminContainer">
+        <div className="AdminDashbord">
+         
+            <div className="book">
+               <div className="Adminprofile">
+            <div className="profilimage">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu5GX1lkI6T4INseXlhyZhaGMtq07LNid9Tw&s" alt="" />
+            </div>
+            <div className="profiletname">
+              <h2>Məleykə Mahmudlu</h2>
+            </div>
+          </div>
+              <div className="booktext">
+                <div className="bookicon">
+                  <FaBook />
+                 <p>book</p>
+              
+              </div>
+              <div className="bookicon">
+                <FaBookAtlas />
+                 <p>e-book</p>
+              </div>
+             <div className="bookicon">
+              <IoIosBook />
+                <p>dövrü mətbuat</p>
+             </div>
+             <div className="bookicon">
+              <IoIosBookmark />
+              <p>e-dövrü-mətbuat</p>
+             </div>
+              </div>
+            
+              
+            </div>
+        </div>
+        <div className="DashboardContent">
+          <table>
+            <thead>
+              <tr>
+                <th>Şəkil</th>
+                <th>Kitabın Adı</th>
+                <th>Müəllifin Adı</th>
+                <th>Yer</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td className='actionbtn'>
+                  <button><MdModeEdit /></button>
+                  <button><AiFillDelete /></button>
+                </td>
+              </tr>
+              <tr>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td className='actionbtn'>
+                  <button><MdModeEdit /></button>
+                  <button><AiFillDelete /></button>
+                </td>
+              </tr>
+              <tr>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td className='actionbtn'>
+                  <button><MdModeEdit /></button>
+                  <button><AiFillDelete /></button>
+                </td>
+              </tr>
+              <tr>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td className='actionbtn'>
+                  <button><MdModeEdit /></button>
+                  <button><AiFillDelete /></button>
+                </td>
+              </tr>
+              <tr>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td className='actionbtn'>
+                  <button><MdModeEdit /></button>
+                  <button><AiFillDelete /></button>
+                </td>
+              </tr>
+              <tr>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td>salam</td>
+                <td className='actionbtn'>
+                  <button><MdModeEdit /></button>
+                  <button><AiFillDelete /></button>
+                </td>
+              </tr>
+              
+             
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Admin
